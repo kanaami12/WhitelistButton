5b4b3dd687e3dc995e7c43b42d86fe39b8078c9c
BuildTools <unconfigured@null.spigotmc.org>
2017-04-09 19:31:55 +0900

    test

--- src/kanaami12/plugin/whitelistbutton/Main.java
+++ src/kanaami12/plugin/whitelistbutton/Main.java
@@ -5,17 +5,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 	
-	public static Main plugin;											//他クラスからのアクセス用
+	public static Main plugin;                                          //他クラスからのアクセス用
 	public static String prefix = ChatColor.GRAY + "[WhitelistButton]";	//プラグインメッセージ用接頭文字列
 	
 	@Override
-	public void onEnable(){	//起動後1度だけ処理されるメソッド
+	public void onEnable(){ //起動後1度だけ処理されるメソッド
 		
-		plugin = this;		//他クラスからMainクラスを扱う
+		plugin = this;      //他クラスからMainクラスを扱う
 		
-		getCommand("whitelistbutton").setExecutor(new MyCommandExecutor(this));		//コマンドを登録(plugin.ymlにコマンドを登録しておかないとエラーが出る)
-		getCommand("whitelistbutton").setTabCompleter(new ConstructTabCompleter());	//タブ補完を登録
-		getServer().getPluginManager().registerEvents(new MyListener(), this);		//イベントリスナを登録
-		this.saveDefaultConfig();													//コンフィグを保存
+		getCommand("whitelistbutton").setExecutor(new MyCommandExecutor(this));     //コマンドを登録(plugin.ymlにコマンドを登録しておかないとエラーが出る)
+		getCommand("whitelistbutton").setTabCompleter(new ConstructTabCompleter()); //タブ補完を登録
+		getServer().getPluginManager().registerEvents(new MyListener(), this);      //イベントリスナを登録
+		this.saveDefaultConfig();                                                   //コンフィグを保存
 	}
 }
